2903acbdaebe9977380ec87ec2711f2cb95ac948
Alexander Solovyov <piranha@piranha.org.ua>
2009-04-02 14:34:28 +0300

    cherrypy server to look at generated content locally

---
+++ lib/server.py
@@ -0,0 +1,33 @@
+import sys, logging
+import os.path as op
+
+logger = logging.getLogger('server')
+
+
+def start_server(address, port, path):
+    try:
+        import cherrypy
+        from cherrypy.lib.static import serve_file
+    except ImportError:
+        logger.fatal('CherryPy is required to run webserver')
+        sys.exit(1)
+
+    class Root:
+        @cherrypy.expose
+        def default(self, *args):
+            fn = op.join(path, *(args + ('index.html', )))
+            if op.exists(fn):
+                return serve_file(fn)
+            raise cherrypy.NotFound
+
+    cherrypy.config.update({'server.socket_host': address,
+                            'server.socket_port': port,
+                            'log.screen': True})
+    conf = {'/static': {
+            'tools.staticdir.dir': op.join(path, 'static'),
+            'tools.staticdir.on': True,
+            }}
+    cherrypy.tree.mount(Root(), '/', conf)
+
+    cherrypy.engine.autoreload.unsubscribe()
+    cherrypy.engine.start()
--- main.py
+++ main.py
@@ -2,15 +2,44 @@
 
 import os, sys, logging
 import os.path as op
-
-logging.basicConfig()
+from optparse import OptionParser
 
 sys.path.insert(0, op.join(op.dirname(op.abspath(__file__)), '..'))
 
 from cyrax.generator import generator
+from cyrax.lib.server import start_server
 
-def main(argv):
-    generator(os.path.abspath('content'), os.path.abspath('deploy'))
+
+def main():
+    usage = '%s [options]' % sys.argv[0]
+    parser = OptionParser(usage)
+    parser.add_option('', '--log', default='-',
+                      help='redirect log to this file (default: stdout)')
+    parser.add_option('-q', '--quiet', default=False, action='store_true',
+                      help='do not output information messages')
+    parser.add_option('-w', '--webserve', default=False, action='store_true',
+                      help='start server instead of site generation')
+    parser.add_option('-a', '--address', default='0.0.0.0',
+                      help='address to listen on (default: all interfaces)')
+    parser.add_option('-p', '--port', default=8000,
+                      help='port to listen on (default: 8000)')
+
+    opts, args = parser.parse_args()
+
+    if len(args):
+        parser.print_help()
+        sys.exit()
+
+    level = opts.quiet and logging.ERROR or logging.INFO
+    if opts.log == '-':
+        logging.basicConfig(stream=sys.stdout, level=level)
+    else:
+        logging.basicConfig(filename=opts.log, level=level)
+
+    if opts.webserve:
+        start_server(opts.address, opts.port, os.path.abspath('deploy'))
+    else:
+        generator(os.path.abspath('content'), os.path.abspath('deploy'))
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
