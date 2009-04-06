b256f6a4bbfcf9bce7365d12861c544694f70929
Alexander Solovyov <piranha@piranha.org.ua>
2009-04-06 18:01:19 +0300

    don't fail on undefined values

--- cyraxlib/env.py
+++ cyraxlib/env.py
@@ -1,6 +1,13 @@
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Undefined
 
 from cyraxlib import typogrify, templatefilters, templatetags
+
+
+class LoyalUnderfined(Undefined):
+    def __getattr__(self, name):
+        return
+
+    __getitem__ = __getattr__
 
 
 def initialize_env(source):
@@ -9,8 +16,9 @@ def initialize_env(source):
     '''
     loader = FileSystemLoader(source)
 
-    env = Environment(loader=loader, extensions=[templatetags.MetaInfoExtension,
-                                                 templatetags.MarkExtension])
+    env = Environment(loader=loader, undefined=LoyalUnderfined,
+                      extensions=[templatetags.MetaInfoExtension,
+                                  templatetags.MarkExtension])
 
     filters = dict((f.__name__, f) for f in typogrify.filters)
     env.filters.update(filters)
