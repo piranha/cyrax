753551a47aa6097383090b31ddffa15016bde912
Cong <congusbongus@gmail.com>
2018-10-26 10:10:09 +1100

    Update rstextensions.py

--- cyrax/template/rstextensions.py
+++ cyrax/template/rstextensions.py
@@ -50,9 +50,9 @@ class Pygments(Directive):
             # no lexer found - use the text one instead of an exception
             lexer = TextLexer()
         # take an arbitrary option if more than one is given
-        variant = self.options and self.options.keys()[0] or 'default'
+        variant = self.options and next(iter(self.options.keys())) or 'default'
         formatter = self.VARIANTS[variant]
-        parsed = highlight(u'\n'.join(self.content), lexer, formatter)
+        parsed = highlight('\n'.join(self.content), lexer, formatter)
         return [nodes.raw('', parsed, format='html')]
 
 try:
